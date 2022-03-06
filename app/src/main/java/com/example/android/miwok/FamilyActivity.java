617a0e93b5a820f91247448fe5641ad20f74332e
stephanie cliett <sclie001@fiu.edu>
2022-03-05 20:33:41 -0500

    added images to each category of words except the phrases category (#1)

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -20,16 +20,26 @@ public class FamilyActivity extends AppCompatActivity {
         //Word w = new Word("one", "lutti");
         //words.add(w);
 
-        words.add(new Word("father", "әpә"));
-        words.add(new Word("mother", "әṭa"));
-        words.add(new Word("son", "angsi"));
-        words.add(new Word("daughter", "tune"));
-        words.add(new Word("older brother", "taachi"));
-        words.add(new Word("younger brother", "chalitti"));
-        words.add(new Word("older sister", "tete"));
-        words.add(new Word("younger sister", "kolliti"));
-        words.add(new Word("grand mother", "ama"));
-        words.add(new Word("grand father", "paapa"));
+        words.add(new Word("father", "әpә",
+                R.drawable.family_father));
+        words.add(new Word("mother", "әṭa",
+                R.drawable.family_mother));
+        words.add(new Word("son", "angsi",
+                R.drawable.family_son));
+        words.add(new Word("daughter", "tune",
+                R.drawable.family_daughter));
+        words.add(new Word("older brother", "taachi",
+                R.drawable.family_older_brother));
+        words.add(new Word("younger brother", "chalitti",
+                R.drawable.family_younger_brother));
+        words.add(new Word("older sister", "tete",
+                R.drawable.family_older_sister));
+        words.add(new Word("younger sister", "kolliti",
+                R.drawable.family_younger_sister));
+        words.add(new Word("grand mother", "ama",
+                R.drawable.family_grandmother));
+        words.add(new Word("grand father", "paapa",
+                R.drawable.family_grandfather));
 
         //int words_size = words.size();
 
@@ -38,7 +48,7 @@ public class FamilyActivity extends AppCompatActivity {
                 new WordAdapter(this, words);
 
         //Find the {@link ListView} object in the view hierarchy of the {@link Activity}
-        ListView listView = (ListView)findViewById(R.id.list);
+        ListView listView = findViewById(R.id.list);
         //use adapter to provide list items when requested by listView as view items are off screen
         listView.setAdapter(adapter);
     }
