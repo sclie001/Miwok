a61efd530b3c221b295bac8c56704683e81cec94
Stephanie <sclie001@fiu.edu>
2022-06-03 14:57:01 -0400

    moved Numbers activity logic into created NumbersFragment, updated Numbers activity to display the new activity_category xml layout

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,67 +18,15 @@ public class NumbersActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
+        setContentView(R.layout.activity_category);
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .replace(R.id.container, new NumbersFragment())
+                    .commit();
+        }
 
-        //create String array of words
-        final ArrayList<Word> words = new ArrayList<>();
 
-        //Word w = new Word("one", "lutti");
-        //words.add(w);
-
-        words.add(new Word("one", "lutti",
-                R.drawable.number_one, R.raw.number_one));
-        words.add(new Word("two", "otiiko",
-                R.drawable.number_two, R.raw.number_two));
-        words.add(new Word("three", "tolookosu",
-                R.drawable.number_three, R.raw.number_three));
-        words.add(new Word("four", "oyyisa",
-                R.drawable.number_four, R.raw.number_four));
-        words.add(new Word("five", "massokka",
-                R.drawable.number_five, R.raw.number_five));
-        words.add(new Word("six", "tommokka",
-                R.drawable.number_six, R.raw.number_six));
-        words.add(new Word("seven", "kenekaku",
-                R.drawable.number_seven, R.raw.number_seven));
-        words.add(new Word("eight", "kawinta",
-                R.drawable.number_eight, R.raw.number_eight));
-        words.add(new Word("nine", "wo'e",
-                R.drawable.number_nine, R.raw.number_nine));
-        words.add(new Word("ten", "na'aacha",
-                R.drawable.number_ten, R.raw.number_ten));
-
-        //int words_size = words.size();
-
-        //holds the list of items and determines the layout by the resource given
-        WordAdapter adapter =
-                new WordAdapter(this, words, R.color.category_numbers);
-
-        //Find the {@link ListView} object in the view hierarchy of the {@link Activity}
-        ListView listView = findViewById(R.id.list);
-        //use adapter to provide list items when requested by listView as view items are off screen
-        listView.setAdapter(adapter);
-
-        // Set a click listener to play the audio when the list item is clicked on
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(), "Playing audio",
-                        Toast.LENGTH_SHORT).show();
-
-                // Get the {@link Word} object at the given position the user clicked on
-                Word currentWord = words.get(i);
-                int audioResourceId = currentWord.getAudio_resource_id();
-
-                // Media Player handles playback of audio files
-                // Create and setup the {@link MediaPlayer} for the audio resource associated
-                // with the current word
-                MediaPlayer mediaPlayer = MediaPlayer.create(NumbersActivity.this,
-                        audioResourceId);
-
-                //start the audio file
-                mediaPlayer.start();
-            }
-        });
 
     }
 }
